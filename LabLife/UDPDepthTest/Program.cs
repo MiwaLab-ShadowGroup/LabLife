00d76d9d3f1ec23efed28ae3e1d81659b1b7155d
Yyamakawa <yy816-b.t.a@ruri.waseda.jp>
2015-10-15 12:54:37 +0900

    最初のコミット　ソリューションを作成

---
+++ LabLife/UDPDepthTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPDepthTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
